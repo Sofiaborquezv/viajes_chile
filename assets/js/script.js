e06b37c397b4b61f49ab9f568035357e85439795
sofiaborquezv <sofborquezvas@gmail.com>
2021-06-24 17:03:47 -0400

    Efecto Toggle en cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
 function getFormContacto(form) {
 alert("Su mail ha sido enviado",form);
-
 }
+$(function () {
+    $('[data-bs-toggle="tooltip"]').tooltip()
+});
+
+  $(".img-top").click(function(){
+    $(".card-text").toggle();
+});
